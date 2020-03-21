ed0c94855bd923db8cac0f8a03bc74d7d5ff2766
Hector Hernan Buitrago Pabon <hector.buitrago@duratex.com.co>
2020-03-21 08:52:50 -0500

    Se subio el linear layout

---
+++ app/src/main/java/com/example/miaplicacion/HorizontalActivity.kt
@@ -0,0 +1,17 @@
+package com.example.miaplicacion
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HorizontalActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_horizontal)
+
+
+
+    }
+
+}
